e6a0c1437f395699a28dce74c8f4a720f77f52bd
Emma <emmanuellebarrueco@orange.fr>
2017-11-14 15:49:11 +0100

    Auto stash before merge of "master" and "origin/master"

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php echo 'Nouakchott' ?>
+    <?php
+    $page = 'index';
+    require_once('parts/header.php');
+    ?>
+
+
+
+    <img src="img/car.jpg" alt="course de voitures">
+	<img src="img/moto2.jpg" alt="course de mots">
+	<img src="img/moto.jpg" alt="autre course à motos">
+
+	<?php require_once('parts/footer.php'); ?>
---
+++ parts/header.php
@@ -0,0 +1,42 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Header</title>
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+</head>
+<body>
+
+<header>
+	<?php echo $titre; ?><h1>Votre salle de sport, </h1><?php if(isset($_SESSION['prenom'])): ?>
+	<nav>
+		<ul>
+			<li <?php if ($page == "index") echo 'class="active"'; ?>><a href="index.php">accueil |</a></li>
+
+			<li <?php if ($page == "presenstation") echo 'class="active"'; ?>><a href="presenstation.php">présentation |</a></li>
+
+			<li <?php if ($page == "inscription") echo 'class="active"'; ?>><a href="inscription.php">inscription |</a></li>
+
+			<li <?php if ($page == "contact") echo 'class="active"'; ?>><a href="contact.php">contact  </a></li>
+
+			<li>
+				<form class="droite" action="connexion.php" method="post">
+					<label for="connexion" id="connexion"><?php if (isset($_SESSION['prenom'])) echo 'Déconnexion'; else echo 'Connexion'; ?> connexion : 
+					</label>
+					<input type="password" name="connexion">
+				</form>
+			</li>
+
+		</ul>
+	</nav>
+	<?php echo $trait; ?><hr>
+</header>
+
+<main>
+
+
+
