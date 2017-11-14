2652b411e5282cfe20f1c4f49e8513d85dd96c6f
AoClem <clem.garnier6@gmail.com>
2017-11-14 15:50:45 +0100

    modifs presenstation.php + ajout salles.php,salle.php et dossier img

---
+++ salles.php
@@ -0,0 +1,28 @@
+<?php
+	$salles = [
+		[
+			'id' => 1,
+			'nom' => 'Yoga',
+			'capacite' => 20,
+			'equipement' => 'Tapis',
+		],
+		[
+			'id' => 2,
+			'nom' => 'Zumba',
+			'capacite' => 30,
+			'equipement' => 'Sono',
+		],
+		[
+			'id' => 3,
+			'nom' => 'Muscu',
+			'capacite' => 15,
+			'equipement' => 'Altères, bancs et poids',
+		],
+		[
+			'id' => 4,
+			'nom' => 'Tapis',
+			'capacite' => 11,
+			'equipement' => '11 tapis de course',
+		]
+	];
+?>
